9e852fc0d66aa2995cada47865389bda8774ae6d
acionescu <adrian.ionescu.consulting@gmail.com>
2012-11-07 17:47:30 +0200

    bla

--- src/main/java/ro/zg/mdb/core/schema/ObjectIdMapper.java
+++ src/main/java/ro/zg/mdb/core/schema/ObjectIdMapper.java
@@ -20,14 +20,22 @@ import ro.zg.mdb.core.annotations.ObjectId;
 import ro.zg.mdb.core.exceptions.MdbException;
 import ro.zg.mdb.core.meta.persistence.data.PersistentFieldMetadataImpl;
 import ro.zg.mdb.core.meta.persistence.data.PersistentObjectMetadataImpl;
+import ro.zg.metadata.annotations.validation.Required;
+import ro.zg.metadata.commons.AnnotationMapper;
+import ro.zg.metadata.commons.AnnotationMapperContext;
+import ro.zg.metadata.commons.FieldMetadataImpl;
+import ro.zg.metadata.exceptions.MetadataException;
 import ro.zg.util.data.GenericNameValue;
 
-public class ObjectIdMapper extends ObjectDataModelAnnotationMapper<ObjectId>{
+public class ObjectIdMapper implements AnnotationMapper<AnnotationMapperContext<Required, PersistentObjectMetadataImpl<?>>>{
 
     @Override
-    public void map(ObjectDataModelAnnotationMapperContext<ObjectId> amc) throws MdbException {
-	PersistentObjectMetadataImpl<?> odm=amc.getObjectDataModel();
+    public void map(
+	    AnnotationMapperContext<Required, PersistentObjectMetadataImpl<?>> amc)
+	    throws MetadataException {
+	PersistentObjectMetadataImpl<?> odm=amc.getMetadata();
 	
+	/* here should only mark the field as object id, and handle the logic of the object model somewhere else */
 	if(odm.getObjectIdField() != null) {
 	    throw new MdbException(MdbErrorType.MULTIPLE_OBEJCT_ID_FIELDS, new GenericNameValue("type",odm.getType()));
 	}
@@ -37,6 +45,22 @@ public class ObjectIdMapper extends ObjectDataModelAnnotationMapper<ObjectId>{
 	}
 	fdm.setObjectId(true);
 	odm.setObjectIdField(fdm);
+	
     }
+
+//    @Override
+//    public void map(ObjectDataModelAnnotationMapperContext<ObjectId> amc) throws MdbException {
+//	PersistentObjectMetadataImpl<?> odm=amc.getObjectDataModel();
+//	
+//	if(odm.getObjectIdField() != null) {
+//	    throw new MdbException(MdbErrorType.MULTIPLE_OBEJCT_ID_FIELDS, new GenericNameValue("type",odm.getType()));
+//	}
+//	PersistentFieldMetadataImpl<String> fdm = (PersistentFieldMetadataImpl<String>)amc.getFieldDataModel();
+//	if(!fdm.getType().equals(String.class)) {
+//	    throw new MdbException(MdbErrorType.WRONG_FIELD_TYPE, new GenericNameValue("objectType",odm.getType()),new GenericNameValue("field",fdm.getName()),new GenericNameValue("expectedType","String"));
+//	}
+//	fdm.setObjectId(true);
+//	odm.setObjectIdField(fdm);
+//    }
 
 }
